16e9d4c270b3d2d0922f97f7ea14210be4f6d954
polyqubit <polyumbrial@gmail.com>
2022-05-15 00:54:24 -0700

    image sdl, funny poglin
    this took too long

--- main.cpp
+++ main.cpp
@@ -1,22 +1,74 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <conio.h>
-// terminal -> make
+// terminal -> command in make.txt
+// following tutorial for now lol
 const int WIDTH = 800, HEIGHT = 600;
 
 int main(int argc, char *argv[]) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_Window *window = SDL_CreateWindow("TestSDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-	if(window == NULL) {std::cout << "error creating window" << SDL_GetError() << std::endl; return 1;}
-
-	SDL_Event windowEvent;
-	while(true) {
-		if(SDL_PollEvent(&windowEvent)) {
-			if(SDL_QUIT == windowEvent.type) {break;}
-		}
+	if(window == NULL) {
+		std::cout << "error creating window" << SDL_GetError() << std::endl;
+		return 1;
 	}
 
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	if(renderer == NULL) {
+		std::cout << "error getting renderer" << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+    if(!window_surface) {
+        std::cout << "error getting window surface" << SDL_GetError() << std::endl;
+		return 1;
+    }
+
+	SDL_Surface *image = IMG_Load("resources/test.png");
+	if(!image) {
+    	std::cout << "error loading image" << SDL_GetError() << std::endl;
+    	return 1;
+	}
+
+	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
+	if(!texture) {
+		std::cout << "error creating texture" << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Rect dimensions = {0, 0, image->w, image->h};
+	SDL_RenderCopy(renderer, texture, NULL, &dimensions);
+
+	bool keep_window_open = true;
+	while(keep_window_open) {
+    	SDL_Event e;
+    	while(SDL_PollEvent(&e) > 0) {
+        	switch(e.type) {
+            	case SDL_QUIT:
+                	keep_window_open = false;
+                break;
+				case SDL_MOUSEBUTTONDOWN:
+					dimensions = {0, 0, 500, image->h};
+					std::cout << "MOUSE BUTTON PRESSED!!!\n";
+				break;
+				case SDL_MOUSEBUTTONUP:
+					dimensions = {0, 0, image->w, image->h};
+					SDL_UpdateWindowSurface(window);
+				break;
+			}
+			SDL_RenderCopy(renderer, texture, NULL, &dimensions);
+			SDL_RenderPresent(renderer);
+        	SDL_BlitSurface(image, NULL, window_surface, NULL);
+        	// SDL_UpdateWindowSurface(window);
+    	}
+	}
+
+	SDL_DestroyTexture(texture);
+	SDL_FreeSurface(image);
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return EXIT_SUCCESS;
